67430c45c061bee95a1b76203ef1913b1bdd8eb3
cuttleman <silluat11@gmail.com>
2021-06-14 16:56:02 +0900

    🛠 postgresql on heroku addons is errors

--- build/api/coffee/deleteCoffeeShop/deleteCoffeeShop.typeDef.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var apollo_server_1 = require("apollo-server");
-exports.default = apollo_server_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  type DeleteCoffeeShop {\n    result: Boolean!\n    error: String\n  }\n  type Mutation {\n    deleteCoffeeShop(id: String!): DeleteCoffeeShop!\n  }\n"], ["\n  type DeleteCoffeeShop {\n    result: Boolean!\n    error: String\n  }\n  type Mutation {\n    deleteCoffeeShop(id: String!): DeleteCoffeeShop!\n  }\n"])));
-var templateObject_1;
